a5d0fac6e357a2b9c6f2387f4fed1b0c1033d290
Javi3861 <javi2cc1@gmail.com>
2023-07-07 21:22:03 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+
+type FormElemet = React.FormEvent<HTMLFormElement>;
+interface ITarea {
+  name: string;
+  done: boolean;
+}
 
 function App() {
+  const [newTexto, setNewTexto] = useState<string>("");
+  const [tarea, setTarea] = useState<ITarea[]>([]);
+
+  const handleSubmit = (e: FormElemet) => {
+    e.preventDefault();
+    addTarea(newTexto);
+    setNewTexto("");
+  };
+
+  const addTarea = (name: string) => {
+    const newTexto = [...tarea, { name, done: false }];
+    setTarea(newTexto);
+  };
+
+  const toggleDonTask = (i:number) => {
+    const newTexto: ITarea[] = [...tarea]
+    newTexto[i].done = !newTexto[i].done
+    setTarea(newTexto)
+  }
+
+  const removeTask = (i:number) => {
+    const newTexto: ITarea[]  = [...tarea]
+    newTexto.splice(i, 1)
+    setTarea(newTexto)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  onChange={(e) => setNewTexto(e.target.value)}
+                  className="form-control"
+                  
+                  autoFocus
+                />
+                <button className="btn btn-success btn-block mt-2">
+                  Enviar
+                </button>
+              </form>
+            </div>
+          </div>
+          {tarea.map((t: ITarea, i: number) => (
+            <div className="card card-body mt-2" key={i}>
+              <h2 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                {t.name}
+              </h2>
+              <div>
+                <button className="btn btn-secondary" onClick={() => toggleDonTask(i)}>
+                  {t.done ? '✓' : '✘'}
+                  </button>
+                  <button className="btn btn-danger" onClick={() => removeTask(i)}>
+                    Eliminar
+                  </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
